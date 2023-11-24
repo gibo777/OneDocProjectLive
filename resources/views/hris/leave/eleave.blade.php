2c02ac5a8758945021e7a80bc400ec13f24b60f7
gibo777 <103085126+gibo777@users.noreply.github.com>
2023-11-24 12:25:13 +0800

    Update for Leave - Others

--- resources/views/hris/leave/eleave.blade.php
+++ resources/views/hris/leave/eleave.blade.php
@@ -473,7 +473,6 @@ $(document).ready(function(){
         }
 
         leaveBalance(); // This will show current Leave Balance/s
-        alert($(this).val().toUpperCase());
 
         if ($(this).val()=="SL" || $(this).val()=="EL" || $(this).val().toUpperCase()=="OTHERS") {
             return true;
