42712e123da41a8dd5b024fccca937b03b407b44
gibo777 <glretiro.1doc@gmail.com>
2024-02-14 15:17:28 +0800

    Add .gitignore to ignore storage/framework/views/

--- storage/framework/views/51d05652624f0379de825a15837562e4a535c8cc.php
+++
@@ -1,5 +0,0 @@
-<button <?php echo e($attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center justify-center px-4 py-2 bg-blue-800 rounded-md font-semibold text-sm text-white uppercase tracking-widest hover:bg-blue-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition'])); ?>>
-    <?php echo e($slot); ?>
-
-</button>
-<?php /**PATH C:\xampp\OneDocProject\htdocs\OneDocProjectLive\resources\views/vendor/jetstream/components/button.blade.php ENDPATH**/ ?>
