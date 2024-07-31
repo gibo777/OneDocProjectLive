07ea14398200827b2c7e86b7e9ac44471a3438e7
gibo777 <103085126+gibo777@users.noreply.github.com>
2024-07-31 09:51:57 +0800

    Update Employees

--- app/Http/Controllers/EmployeesController.php
+++ app/Http/Controllers/EmployeesController.php
@@ -185,7 +185,7 @@ class EmployeesController extends Controller
                 'weekly_schedule'   => join('|',$request->update_weekly_schedule),
                 'office'            => $request->office,
                 'supervisor'        => $request->supervisor,
-                'date_regularized'  => date('Y-m-d',strtotime($request->dateRegularized)),
+                'date_regularized'  => $request->dateRegularized ? date('Y-m-d',strtotime($request->dateRegularized)) : null,
                 'updated_at'        => Carbon::now('Asia/Manila'),
                 'updated_by'        => Auth::user()->employee_id
             );
