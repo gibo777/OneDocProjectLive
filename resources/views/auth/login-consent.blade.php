199e7ced9f1b3fd48437edc52076d6e3b6719b99
gibo777 <103085126+gibo777@users.noreply.github.com>
2025-07-17 17:43:27 +0800

    Timekeeping, Geolocation, and Login Consent

--- resources/views/auth/login-consent.blade.php
+++ resources/views/auth/login-consent.blade.php
@@ -1,219 +1,236 @@
 <div class="text-justify px-2 text-sm" style="max-height: 500px; overflow-y: auto;">
-<p class="text-center h6">One Document Corporation Web Application Data Privacy Consent Form</p>
-<p>1. Introduction:</p>
-<p>
-One Document Corporation recognizes and respects the fundamental right to privacy and security.
-As an Information Technology (IT) and Project Management service provider, the company is
-committed to safeguarding the personal data of its employees and affiliates. This document outlines
-One Document Corporation’s Data Privacy Policy for the Timekeeping, e-Leave Application, and eLeave Approval Modules.</p>
-<p>1.1 Scope:</p>
-<p>This policy applies to all employees under One Document Corporation for Timekeeping and Leave
-Application verification and approval.</p>
-<p>1.2 What Data Do We Collect and Why?</p>
-<p>We intend to collect and process the following data necessary for:</p>
+    <p class="text-center h6">One Document Corporation Web Application Data Privacy Consent Form</p>
+    <p>1. Introduction:</p>
+    <p>
+        One Document Corporation recognizes and respects the fundamental right to privacy and security.
+        As an Information Technology (IT) and Project Management service provider, the company is
+        committed to safeguarding the personal data of its employees and affiliates. This document outlines
+        One Document Corporation’s Data Privacy Policy for the Timekeeping, e-Leave Application, and eLeave Approval
+        Modules.</p>
+    <p>1.1 Scope:</p>
+    <p>This policy applies to all employees under One Document Corporation for Timekeeping and Leave
+        Application verification and approval.</p>
+    <p>1.2 What Data Do We Collect and Why?</p>
+    <p>We intend to collect and process the following data necessary for:</p>
 
-<table class="w-full text-center text-xs table-hover table-striped">
-<thead>
-<tr style="font-size: 11px;">
-<th scope="col" style="width: 60%;">Fields</th>
-<th scope="col" style="width: 22%;">Data</th>
-{{-- <th scope="col" style="width: 18%;">Classification Purpose</th> --}}
-</tr>
-</thead>
+    <table class="w-full text-center text-xs table-hover table-striped">
+        <thead>
+            <tr style="font-size: 11px;">
+                <th scope="col" style="width: 60%;">Fields</th>
+                <th scope="col" style="width: 22%;">Data</th>
+                {{-- <th scope="col" style="width: 18%;">Classification Purpose</th> --}}
+            </tr>
+        </thead>
 
-<tbody class="h7">
+        <tbody class="h7">
 
-<tr>
-<td class="text-left pl-2">
-Last Name, First Name, Middle Name, Ext. (Extension), Uploaded Photo, Birthdate, Birth Place
-</td>
-<td>Personal</td>
-{{-- <td>2, 6</td> --}}
-</tr>
+            <tr>
+                <td class="text-left pl-2">
+                    Last Name, First Name, Middle Name, Ext. (Extension), Uploaded Photo, Birthdate, Birth Place
+                </td>
+                <td>Personal</td>
+                {{-- <td>2, 6</td> --}}
+            </tr>
 
-<tr>
-<td class="text-left pl-2">
-Employee ID Number
-</td>
-<td>Personal</td>
-{{-- <td>10, 18</td> --}}
-</tr>
+            <tr>
+                <td class="text-left pl-2">
+                    Employee ID Number
+                </td>
+                <td>Personal</td>
+                {{-- <td>10, 18</td> --}}
+            </tr>
 
-<tr>
-<td class="text-left pl-2">
-Department
-</td>
-<td>Personal</td>
-{{-- <td>7, 14</td> --}}
-</tr>
+            <tr>
+                <td class="text-left pl-2">
+                    Department
+                </td>
+                <td>Personal</td>
+                {{-- <td>7, 14</td> --}}
+            </tr>
 
-<tr>
-<td class="text-left pl-2">
-Position
-</td>
-<td>Personal</td>
-{{-- <td>16</td> --}}
-</tr>
+            <tr>
+                <td class="text-left pl-2">
+                    Position
+                </td>
+                <td>Personal</td>
+                {{-- <td>16</td> --}}
+            </tr>
 
-<tr>
-<td class="text-left pl-2">
-Country, Province, City/Municipality, Barangay, House No./Street, Zip Code
-</td>
-<td>Personal</td>
-{{-- <td>19</td> --}}
-</tr>
+            <tr>
+                <td class="text-left pl-2">
+                    Country, Province, City/Municipality, Barangay, House No./Street, Zip Code
+                </td>
+                <td>Personal</td>
+                {{-- <td>19</td> --}}
+            </tr>
 
-<tr>
-<td class="text-left pl-2">
-Email
-</td>
-<td>Personal</td>
-{{-- <td>17 (9)</td> --}}
-</tr>
+            <tr>
+                <td class="text-left pl-2">
+                    Email
+                </td>
+                <td>Personal</td>
+                {{-- <td>17 (9)</td> --}}
+            </tr>
 
-<tr>
-<td class="text-left pl-2">
-Contact Number, Mobile Number
-</td>
-<td>Personal</td>
-{{-- <td>8</td> --}}
-</tr>
+            <tr>
+                <td class="text-left pl-2">
+                    Contact Number, Mobile Number
+                </td>
+                <td>Personal</td>
+                {{-- <td>8</td> --}}
+            </tr>
 
-<tr>
-<td class="text-left pl-2">
-Sex
-</td>
-<td>Personal</td>
-{{-- <td>15</td> --}}
-</tr>
+            <tr>
+                <td class="text-left pl-2">
+                    Sex
+                </td>
+                <td>Personal</td>
+                {{-- <td>15</td> --}}
+            </tr>
 
-<tr>
-<td class="text-left pl-2">
-Civil Status
-</td>
-<td>Personal</td>
-{{-- <td>13, 14</td> --}}
-</tr>
+            <tr>
+                <td class="text-left pl-2">
+                    Civil Status
+                </td>
+                <td>Personal</td>
+                {{-- <td>13, 14</td> --}}
+            </tr>
 
-<tr>
-<td class="text-left pl-2">
-Nationality
-</td>
-<td>Personal</td>
-{{-- <td>11</td> --}}
-</tr>
+            <tr>
+                <td class="text-left pl-2">
+                    Nationality
+                </td>
+                <td>Personal</td>
+                {{-- <td>11</td> --}}
+            </tr>
 
-<tr>
-<td class="text-left pl-2">
-Employment Status, Date Started
-</td>
-<td>Personal</td>
-{{-- <td>11, 14</td> --}}
-</tr>
+            <tr>
+                <td class="text-left pl-2">
+                    Employment Status, Date Started
+                </td>
+                <td>Personal</td>
+                {{-- <td>11, 14</td> --}}
+            </tr>
 
-<tr>
-<td class="text-left pl-2">
-Leaves: Vacation Leaves, Sick Leaves, Maternity Leaves, Paternity Leaves, Emergency Leaves, Other Leaves
-</td>
-<td>Accounting</td>
-{{-- <td>14</td> --}}
-</tr>
+            <tr>
+                <td class="text-left pl-2">
+                    Leaves: Vacation Leaves, Sick Leaves, Maternity Leaves, Paternity Leaves, Emergency Leaves, Other
+                    Leaves
+                </td>
+                <td>Accounting</td>
+                {{-- <td>14</td> --}}
+            </tr>
 
-<tr>
-<td class="text-left pl-2">
-SSS Number, PHIC Number, Pag-Ibig Number, TIN Number
-</td>
-<td>Accounting</td>
-{{-- <td>11 (20)</td> --}}
-</tr>
+            <tr>
+                <td class="text-left pl-2">
+                    SSS Number, PHIC Number, Pag-Ibig Number, TIN Number
+                </td>
+                <td>Accounting</td>
+                {{-- <td>11 (20)</td> --}}
+            </tr>
 
-<tr>
-<td class="text-left pl-2">
-List of Dependent(s)
-</td>
-<td>Accounting</td>
-{{-- <td>12</td> --}}
-</tr>
+            <tr>
+                <td class="text-left pl-2">
+                    List of Dependent(s)
+                </td>
+                <td>Accounting</td>
+                {{-- <td>12</td> --}}
+            </tr>
 
-<tr>
-<td class="text-left pl-2">
-Health Card Number
-</td>
-<td>Accounting</td>
-{{-- <td>1, 2</td> --}}
-</tr>
+            <tr>
+                <td class="text-left pl-2">
+                    Health Card Number
+                </td>
+                <td>Accounting</td>
+                {{-- <td>1, 2</td> --}}
+            </tr>
 
-<tr>
-<td class="text-left pl-2">
-Driver’s License
-</td>
-<td>Accounting</td>
-{{-- <td>3</td> --}}
-</tr>
+            <tr>
+                <td class="text-left pl-2">
+                    Driver’s License
+                </td>
+                <td>Accounting</td>
+                {{-- <td>3</td> --}}
+            </tr>
 
-<tr>
-<td class="text-left pl-2">
-Passport Number, Expiration Date
-</td>
-<td>Accounting</td>
-{{-- <td>5</td> --}}
-</tr>
+            <tr>
+                <td class="text-left pl-2">
+                    Passport Number, Expiration Date
+                </td>
+                <td>Accounting</td>
+                {{-- <td>5</td> --}}
+            </tr>
 
-<tr>
-<td class="text-left pl-2">
-PRC Number
-</td>
-<td>Accounting</td>
-{{-- <td>2, 4</td> --}}
-</tr>
+            <tr>
+                <td class="text-left pl-2">
+                    PRC Number
+                </td>
+                <td>Accounting</td>
+                {{-- <td>2, 4</td> --}}
+            </tr>
 
-</tbody>
-</table>
-<p class="mt-2">1.3 Data Usage and Disclosure:</p>
-<p>One Document Corporation will not use or disclose the data for purposes other than what it was
-collected for, except with consent or as required by Philippine laws. Information may be shared with
-trusted third parties such as legal advisors, regulatory bodies, and their authorized representatives.</p>
-<p>1.4 Data Security:</p>
-<p>One Document Corporation employs measures to safeguard the data collected, including user
-restriction levels and training. All data that is captured will be stored in an encrypted database.</p>
-<p>1.5 Data Retention Policy:</p>
-<p>Information is retained only as necessary to provide for the purposes outlined in this policy or as
-required under Philippine laws and regulations. When the data is no longer needed, it will be
-securely deleted.</p>
-<p>1.5.1 Uploaded Identification Photo:</p>
-<p>Identification photo refers to the formal photos required for upload. These ID photos will be
-uploaded into the system for Personal Data and will be retained for up to three (3) years after the
-separation of the employee from the company.</p>
-<p>1.5.2 Employee Record:</p>
-<p>Employee Record refers to the overall collated information collected from the employees. The
-Employee Record will be retained for up to three (3) years after the separation of the employee
-from the company.</p>
-<p>1.5.3 Biometric Images:</p>
-<p>Biometric images refer to images captured during Timekeeping, to verify accuracy and truthfulness.
-These images will be retained for six (6) months.</p>
-<p>1.6 Rights of Data Subjects:</p>
-<p>One Document Corporation employees have the right to access, amend, and delete the data
-collected and stored by the company in this policy. Requests should be made formally to the One
-Document Corporation HR Department, and the HR Department will respond by applicable laws and
-regulations.</p>
-<p>1.7 Changes to the Policy:</p>
-<p>One Document Corporation reserves the right to update this policy as deemed necessary due to
-legal requirements, new enhancements, or changes in processes within the HRIS Employee Portal
-and its modules. In the event of an update, such changes will be communicated to the employees
-through an inter-company memo, along with the updated version of this policy.</p>
-<p>1.8 Contact Information:</p>
-<p>For inquiries and concerns related to the contents of this policy or your collected data, please
-contact our Data Protection Officer at <link>dpo@onedoc.ph</link>.</p>
-<p>1.9 Acknowledgment:</p>
+            <tr>
+                <td class="text-left pl-2">
+                    Location (Longitude and Latitude)
+                </td>
+                <td>Personal</td>
+                {{-- <td>2, 4</td> --}}
+            </tr>
 
-<p>By using One Document Corporation’s HRIS Employee Portal, you:</p>
-<p class="pl-3">- Acknowledge that you have read, understood, and agreed to the information and practices outlined
-in this Data Privacy Policy.</p>
-<p class="pl-3">- Are allowing One Document Corporation to collect and process the data for its intended use as
-stipulated in this Data Privacy Policy.</p>
+        </tbody>
+    </table>
+    <p class="mt-2">1.3 Data Usage and Disclosure:</p>
+    <p>One Document Corporation will not use or disclose the data for purposes other than what it was
+        collected for, except with consent or as required by Philippine laws. Information may be shared with
+        trusted third parties such as legal advisors, regulatory bodies, and their authorized representatives.</p>
+    <p>1.4 Data Security:</p>
+    <p>One Document Corporation employs measures to safeguard the data collected, including user
+        restriction levels and training. All data that is captured will be stored in an encrypted database.</p>
+    <p>1.5 Data Retention Policy:</p>
+    <p>Information is retained only as necessary to provide for the purposes outlined in this policy or as
+        required under Philippine laws and regulations. When the data is no longer needed, it will be
+        securely deleted.</p>
+    <p>1.5.1 Uploaded Identification Photo:</p>
+    <p>Identification photo refers to the formal photos required for upload. These ID photos will be
+        uploaded into the system for Personal Data and will be retained for up to three (3) years after the
+        separation of the employee from the company.</p>
+    <p>1.5.2 Employee Record:</p>
+    <p>Employee Record refers to the overall collated information collected from the employees. The
+        Employee Record will be retained for up to three (3) years after the separation of the employee
+        from the company.</p>
+    <p>1.5.3 Biometric Images:</p>
+    <p>Biometric images refer to images captured during Timekeeping, to verify accuracy and truthfulness.
+        These images will be retained for six (6) months.</p>
+
+    <p>1.5.4 Geo location Record</p>
+    <p> location refers to location capture during Time-in and Time-out during timekeeping, to verify the
+        location of the employee during time logging.</p>
+
+    <p>1.6 Rights of Data Subjects:</p>
+    <p>One Document Corporation employees have the right to access, amend, and delete the data
+        collected and stored by the company in this policy. Requests should be made formally to the One
+        Document Corporation HR Department, and the HR Department will respond by applicable laws and
+        regulations.</p>
+    <p>1.7 Changes to the Policy:</p>
+    <p>One Document Corporation reserves the right to update this policy as deemed necessary due to
+        legal requirements, new enhancements, or changes in processes within the HRIS Employee Portal
+        and its modules. In the event of an update, such changes will be communicated to the employees
+        through an inter-company memo, along with the updated version of this policy.</p>
+    <p>1.8 Contact Information:</p>
+    <p>For inquiries and concerns related to the contents of this policy or your collected data, please
+        contact our Data Protection Officer at
+        <link>dpo@onedoc.ph</link>.
+    </p>
+    <p>1.9 Acknowledgment:</p>
+
+    <p>By using One Document Corporation’s HRIS Employee Portal, you:</p>
+    <p class="pl-3">- Acknowledge that you have read, understood, and agreed to the information and practices outlined
+        in this Data Privacy Policy.</p>
+    <p class="pl-3">- Are allowing One Document Corporation to collect and process the data for its intended use as
+        stipulated in this Data Privacy Policy.</p>
 </div>
 
 <label class="text-sm mt-2 pb-0 mb-0">
-	<input type="checkbox" id="loginCheckbox" class="mr-2"> 
-	I confirm my login (Login consent will expire after 30 days)
+    <input type="checkbox" id="loginCheckbox" class="mr-2">
+    I confirm my login (Login consent will expire after 30 days)
 </label>
