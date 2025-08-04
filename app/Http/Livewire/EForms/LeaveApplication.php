0ae13d877a103c2cf8485c3889363dadc08227c4
gibo777 <103085126+gibo777@users.noreply.github.com>
2025-08-04 16:08:04 +0800

    Send API to HRIS

--- app/Http/Livewire/EForms/LeaveApplication.php
+++ app/Http/Livewire/EForms/LeaveApplication.php
@@ -551,7 +551,7 @@ class LeaveApplication extends Component
                 return $q->whereNull('l.is_cancelled')
                     ->orWhere('l.is_cancelled', '!=', 1);
             })
-            ->where('l.control_number', '2025-1D-IT-3562')
+            // ->where('l.control_number', '2025-1D-IT-3562')
             ->get();
 
         // Log::info('Leave data:', $leaves->toArray());
