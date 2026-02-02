2c315e13a84fa3c893def27730e20992b0f01ee8
gibo777 <103085126+gibo777@users.noreply.github.com>
2026-02-02 17:21:37 +0800

    Company logos (1Doc, Sappi, 1Food, E-Agro)

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,37 +1,56 @@
 <link rel="shortcut icon" href="{{ asset('img/all/onedoc-favicon.png') }}">
 <x-guest-layout>
-<style type="text/css">
-    /* Default style for SweetAlert */
-    /*.swal2-popup {
+    <style type="text/css">
+        /* Default style for SweetAlert */
+        /*.swal2-popup {
       width: 62% !important;
     }*/
 
-    /* Media query for mobile devices */
-    @media (max-width: 767px) {
-      .swal2-popup {
-        width: 100% !important;
-      }
-    }
-    .custom-title-class {
-      font-size: 18px; /* Adjust the font size as needed */
-    }
-    .text-xs {
-      font-size: 0.75rem; /* Adjust the font size as needed */
-    }
-    td {
-      text-transform: none !important;
-    }
-    .swal2-popup {
-      height: 100% !important;
-    }
-</style>
+        /* Media query for mobile devices */
+        @media (max-width: 767px) {
+            .swal2-popup {
+                width: 100% !important;
+            }
+        }
+
+        .custom-title-class {
+            font-size: 18px;
+            /* Adjust the font size as needed */
+        }
+
+        .text-xs {
+            font-size: 0.75rem;
+            /* Adjust the font size as needed */
+        }
+
+        td {
+            text-transform: none !important;
+        }
+
+        .swal2-popup {
+            height: 100% !important;
+        }
+    </style>
 
     <x-jet-authentication-card>
         <x-slot name="logo">
         </x-slot>
-            <div class="">
-                <img src="{{ asset('/img/company/onedoc-logo.png') }}" class="rounded mx-auto d-block pb-3"/>
+        <div class="flex justify-start items-center py-2 space-x-2">
+            <div class="flex justify-center items-center">
+                <img class="h-16 md:h-20 lg:h-24 object-contain" src="{{ asset('/img/company/1doc-logo-100px.jpg') }}" />
             </div>
+            <div class="flex justify-center items-center">
+                <img class="h-16 md:h-20 lg:h-24 object-contain" src="{{ asset('/img/company/sappi-logo-90px.jpg') }}" />
+            </div>
+            <div class="flex justify-center items-center">
+                <img class="h-10 md:h-16 lg:h-20 object-contain"
+                    src="{{ asset('/img/company/1food-logo-90px.jpg') }}" />
+            </div>
+            <div class="flex justify-center items-center">
+                <img class="h-12 md:h-16 lg:h-20 object-contain"
+                    src="{{ asset('/img/company/eagro-logo-100px.jpg') }}" />
+            </div>
+        </div>
 
         <x-jet-validation-errors class="mb-4" />
 
@@ -45,30 +64,34 @@
 
             <div class="text-left">
                 <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="off" />
+                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"
+                    required autofocus autocomplete="off" />
             </div>
 
             <div class="mt-4 text-left">
                 <x-jet-label for="password" value="{{ __('Password') }}" />
                 <div class="input-group mb-3">
-                  <input name="password" type="password" value="" class="input form-control " id="password" placeholder="Password" required="true" aria-label="password" aria-describedby="basic-addon1" autocomplete="current-password"/>
-                  <div class="input-group-append hover" id="eye_view">
-                    <span class="input-group-text">
-                      <i class="fas fa-eye hover" id="show_eye"></i>
-                      <i class="fas fa-eye-slash d-none" id="hide_eye"></i>
-                    </span>
-                  </div>
+                    <input name="password" type="password" value="" class="input form-control " id="password"
+                        placeholder="Password" required="true" aria-label="password" aria-describedby="basic-addon1"
+                        autocomplete="current-password" />
+                    <div class="input-group-append hover" id="eye_view">
+                        <span class="input-group-text">
+                            <i class="fas fa-eye hover" id="show_eye"></i>
+                            <i class="fas fa-eye-slash d-none" id="hide_eye"></i>
+                        </span>
+                    </div>
                 </div>
             </div>
 
             <div class="flex items-center justify-center py-0">
-                <div id="capsLockMessage" class="mt-2 text-md font-weight-bold text-red-500 hidden">Caps Lock is ON</div>
+                <div id="capsLockMessage" class="mt-2 text-md font-weight-bold text-red-500 hidden">Caps Lock is ON
+                </div>
             </div>
 
             <div class="flex items-center justify-center">
                 @if (Route::has('password.request'))
-
-                    <a class="underline text-md text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+                    <a class="underline text-md text-gray-600 hover:text-gray-900"
+                        href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
                     </a>
                 @endif
@@ -90,163 +113,172 @@
 
 
 
-<audio controls autoplay hidden>
-  <!-- <source src="horse.ogg" type="audio/ogg"> -->
-  {{-- <source id="audio-background" src="{{ asset('media/videoplayback.mp3') }}" type="audio/mpeg"> --}}
-</audio>
-<input type="button" id="audio-button" name="" value="button" hidden>
-<script type="text/javascript">
+    <audio controls autoplay hidden>
+        <!-- <source src="horse.ogg" type="audio/ogg"> -->
+        {{-- <source id="audio-background" src="{{ asset('media/videoplayback.mp3') }}" type="audio/mpeg"> --}}
+    </audio>
+    <input type="button" id="audio-button" name="" value="button" hidden>
+    <script type="text/javascript">
+        document.addEventListener('DOMContentLoaded', function() {
+            var capsLockMessage = document.getElementById('capsLockMessage');
 
-document.addEventListener('DOMContentLoaded', function() {
-    var capsLockMessage = document.getElementById('capsLockMessage');
+            function checkCapsLock(e) {
+                var capsLockState = (e.getModifierState && e.getModifierState('CapsLock')) ||
+                    (e.key === 'CapsLock');
 
-    function checkCapsLock(e) {
-        var capsLockState = (e.getModifierState && e.getModifierState('CapsLock')) ||
-                            (e.key === 'CapsLock');
-
-        if (capsLockState) {
-            capsLockMessage.classList.remove('hidden');
-        } else {
-            capsLockMessage.classList.add('hidden');
-        }
-    }
-
-    // Add event listeners to document for keydown and keyup events
-    document.addEventListener('keydown', checkCapsLock);
-    document.addEventListener('keyup', checkCapsLock);
-    document.addEventListener('mouseover', checkCapsLock);
-});
-
-
-
-    $(document).ready(function(){
-
-        $("#eye_view").on('click', function(e) {
-            var x = $("#password");
-            var show_eye = $("#show_eye");
-            var hide_eye = $("#hide_eye");
-            if (x.prop('type') === "password") {
-                x.prop('type','text');
-                show_eye.addClass("d-none");
-                hide_eye.removeClass("d-none");
-            } else {
-                x.prop('type','password');
-                show_eye.removeClass("d-none");
-                hide_eye.addClass("d-none");
+                if (capsLockState) {
+                    capsLockMessage.classList.remove('hidden');
+                } else {
+                    capsLockMessage.classList.add('hidden');
+                }
             }
-          // return false;
+
+            // Add event listeners to document for keydown and keyup events
+            document.addEventListener('keydown', checkCapsLock);
+            document.addEventListener('keyup', checkCapsLock);
+            document.addEventListener('mouseover', checkCapsLock);
         });
 
-        $(document).on('click', '#btnLogin', function() {
 
-            $.ajaxSetup({
-                headers: {
-                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+
+        $(document).ready(function() {
+
+            $("#eye_view").on('click', function(e) {
+                var x = $("#password");
+                var show_eye = $("#show_eye");
+                var hide_eye = $("#hide_eye");
+                if (x.prop('type') === "password") {
+                    x.prop('type', 'text');
+                    show_eye.addClass("d-none");
+                    hide_eye.removeClass("d-none");
+                } else {
+                    x.prop('type', 'password');
+                    show_eye.removeClass("d-none");
+                    hide_eye.addClass("d-none");
                 }
+                // return false;
             });
-            $.ajax({
-                url: '/login-auth',
-                method: 'get',
-                data: { 'email': $('#email').val(), 'password': $('#password').val() }, // prefer use serialize method
-                success:function(auth){
-                    if (auth.isSuccess) {
-                        $.ajax({
-                            url: '/login-consent',
-                            method: 'get',
-                            data: { 'email': $('#email').val() }, // prefer use serialize method
-                            success:function(count){
-                                if (count==0) {
-                                    loginConsentForm ($('#email').val(), $('#password').val());
-                                } else {
-                                    $('#loginForm').submit();
+
+            $(document).on('click', '#btnLogin', function() {
+
+                $.ajaxSetup({
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    }
+                });
+                $.ajax({
+                    url: '/login-auth',
+                    method: 'get',
+                    data: {
+                        'email': $('#email').val(),
+                        'password': $('#password').val()
+                    }, // prefer use serialize method
+                    success: function(auth) {
+                        if (auth.isSuccess) {
+                            $.ajax({
+                                url: '/login-consent',
+                                method: 'get',
+                                data: {
+                                    'email': $('#email').val()
+                                }, // prefer use serialize method
+                                success: function(count) {
+                                    if (count == 0) {
+                                        loginConsentForm($('#email').val(), $(
+                                            '#password').val());
+                                    } else {
+                                        $('#loginForm').submit();
+                                    }
                                 }
-                            }
-                        });
-                    } else {
-                        Swal.fire({ icon: 'error', title: auth.message });
+                            });
+                        } else {
+                            Swal.fire({
+                                icon: 'error',
+                                title: auth.message
+                            });
+                        }
                     }
-                }
+                });
+                return false;
             });
-            return false;
+
+
+
+            function loginConsentForm(email, password) {
+                var originalWidth = $('.swal2-popup').css('width');
+                var originalHeight = $('.swal2-popup').css('height');
+                var loginConsentForm = `@include('auth/login-consent')`;
+
+                Swal.fire({
+                    title: 'Employee Portal Login Consent',
+                    customClass: {
+                        title: 'custom-title-class',
+                    },
+                    html: loginConsentForm,
+                    width: '62%',
+                    // icon: 'question',
+                    showCancelButton: true,
+                    confirmButtonText: 'Login',
+                    cancelButtonText: 'Cancel',
+                    reverseButtons: true,
+                    allowOutsideClick: false,
+                    preConfirm: () => {
+                        // Check the state of the checkbox
+                        if ($('#loginCheckbox').prop('checked')) {
+                            $.ajaxSetup({
+                                headers: {
+                                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                                }
+                            });
+                            $.ajax({
+                                url: '/login-consent',
+                                method: 'post',
+                                data: {
+                                    'email': email,
+                                    'password': password
+                                }, // prefer use serialize method
+                                success: function(data) {
+                                    if (data.isSuccess == false) {
+                                        Swal.fire({
+                                            title: 'Notification',
+                                            icon: 'error',
+                                            text: data.message,
+                                        }).then((childResult) => {
+                                            if (childResult.isConfirmed) {
+                                                Swal.close();
+                                            }
+                                        });
+                                    } else {
+                                        $('#loginForm').submit();
+                                    }
+                                }
+                            });
+                            return false;
+                            // User confirmed login, proceed with the form submission
+
+                        } else {
+                            // User did not confirm login
+                            Swal.showValidationMessage('Please confirm your login');
+                            return false;
+                        }
+                    }
+                }).then((result) => {
+                    // Handling additional actions after the modal is closed (if needed)
+                    if (result.dismiss === Swal.DismissReason.cancel) {
+                        // User clicked "Cancel" or closed the modal
+                        Swal.fire({
+                            title: 'Cancelled',
+                            text: 'Login cancelled',
+                            icon: 'info',
+                            customClass: {
+                                title: 'custom-title-class',
+                            },
+                            width: originalWidth, // Set the original width
+                            height: originalHeight, // Set the original height
+                        });
+                    }
+                });
+            }
         });
-
-
-
-        function loginConsentForm (email, password) {
-            var originalWidth = $('.swal2-popup').css('width');
-            var originalHeight = $('.swal2-popup').css('height');
-            var loginConsentForm = `@include('auth/login-consent')`;
-
-            Swal.fire({
-                title: 'Employee Portal Login Consent',
-                customClass: {
-                    title: 'custom-title-class',
-                },
-                html: loginConsentForm,
-                width: '62%',
-                // icon: 'question',
-                showCancelButton: true,
-                confirmButtonText: 'Login',
-                cancelButtonText: 'Cancel',
-                reverseButtons: true,
-                allowOutsideClick: false,
-                preConfirm: () => {
-                    // Check the state of the checkbox
-                    if ($('#loginCheckbox').prop('checked')) {
-                        $.ajaxSetup({
-                            headers: {
-                                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                            }
-                        });
-                        $.ajax({
-                            url: '/login-consent',
-                            method: 'post',
-                            data: { 'email': email , 'password': password }, // prefer use serialize method
-                            success:function(data){
-                                if (data.isSuccess==false) 
-                                {
-                                    Swal.fire({
-                                        title: 'Notification',
-                                        icon: 'error',
-                                        text: data.message,
-                                    }).then((childResult) => {
-                                        if (childResult.isConfirmed) {
-                                            Swal.close();
-                                        }
-                                    });
-                                } else { $('#loginForm').submit(); }
-                            }
-                        }); return false;
-                        // User confirmed login, proceed with the form submission
-                        
-                    } else {
-                        // User did not confirm login
-                        Swal.showValidationMessage('Please confirm your login');
-                        return false;
-                    }
-                }
-            }).then((result) => {
-                // Handling additional actions after the modal is closed (if needed)
-                if (result.dismiss === Swal.DismissReason.cancel) {
-                    // User clicked "Cancel" or closed the modal
-                    Swal.fire({
-                        title: 'Cancelled',
-                        text: 'Login cancelled',
-                        icon: 'info',
-                        customClass: {
-                            title: 'custom-title-class',
-                        },
-                        width: originalWidth, // Set the original width
-                        height: originalHeight, // Set the original height
-                    });
-                }
-            });
-        }
-    });
-
-</script>
+    </script>
 
 </x-guest-layout>
-
-
-
