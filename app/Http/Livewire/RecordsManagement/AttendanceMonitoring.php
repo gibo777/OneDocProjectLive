f3047f87293ee041111c78e979c7508cbb4b9518
gibo777 <103085126+gibo777@users.noreply.github.com>
2026-02-05 10:22:33 +0800

    Update Attendance Monitoring

--- app/Http/Livewire/RecordsManagement/AttendanceMonitoring.php
+++ app/Http/Livewire/RecordsManagement/AttendanceMonitoring.php
@@ -30,13 +30,13 @@ class AttendanceMonitoring extends Component
 
     public function mount()
     {
-    	$this->fTLdtFrom = now()->toDateString();
+        $this->fTLdtFrom = now()->toDateString();
         $this->loadDropdowns();
     }
 
     public function render()
     {
-        $timeLogs = $this->fetchTimeLogs();
+        $timeLogs = $this->fetchTimelogsAndLeaves();
         $this->loadDropdowns();
 
         return view('livewire.records-management.attendance-monitoring', [
@@ -53,157 +53,71 @@ class AttendanceMonitoring extends Component
         $this->departments = DB::table('departments')->orderBy('department')->get();
     }
 
-    
+
     public function clearDateFilters()
     {
         $this->fTLdtFrom = null;
     }
 
-    private function fetchTimeLogs()
-	{
-		/*$timeLogs = DB::table('users as u')
-        ->select(
-            'th.id',
-            'th.full_name',
-            'th.employee_id',
-            'o.company_name as office',
-            'd.department',
-            'th.log_date',
-            DB::raw("(SELECT control_number FROM leaves WHERE employee_id=th.employee_id AND '$this->fTLdtFrom' BETWEEN date_from AND date_to) AS control_number"),
-            DB::raw("(SELECT leave_type FROM leaves WHERE employee_id=th.employee_id AND '$this->fTLdtFrom' BETWEEN date_from AND date_to) AS leave_type"),
-            DB::raw("(SELECT leave_status FROM leaves WHERE employee_id=th.employee_id AND '$this->fTLdtFrom' BETWEEN date_from AND date_to) AS leave_status"),
-            'th.time_in',
-            'th.time_out',
-            'th.supervisor'
-        )
-        ->leftJoin('time_logs_header as th', 'u.employee_id', '=', 'th.employee_id')
-        ->leftJoin('departments as d', 'th.department', '=', 'd.department_code')
-        ->leftJoin('offices as o', 'th.office', '=', 'o.id')
-        ->when(!empty($this->fTLdtFrom), fn($q) => $q->where('th.log_date', $this->fTLdtFrom))
-
-        ->when(!in_array(Auth::user()->role_type, ['SUPER ADMIN', 'ADMIN']), function ($q) {
-            $q->where(fn($query) => 
-                $query->where('th.employee_id', Auth::user()->employee_id)
-                      ->orWhere('u.supervisor', Auth::user()->employee_id)
-            );
-        })
-        ->when(!in_array(Auth::user()->id, [1, 2]), fn($q) => $q->where('u.id', '!=', 1))
-        ->when(Auth::user()->is_head == 1, function ($q) {
-            $q->where(fn($query) => 
-                $query->where('u.employee_id', Auth::user()->employee_id)
-                      ->orWhere('u.supervisor', Auth::user()->employee_id)
-            );
-        }, fn($q) => 
-            $q->where('u.employee_id', Auth::user()->employee_id)
-        )
-        
-        ->where(fn($q) => $q->where('u.is_deleted', 0)->orWhereNull('u.is_deleted'))
-        ->union(
-            DB::table('users as u')
+    private function fetchTimelogsAndLeaves()
+    {
+        $timeLogs = DB::table('users as u')
             ->select(
-                DB::raw("NULL as id"),
-                'l.name',
+                DB::raw("COALESCE(th.id, NULL) as id"),
+                'u.name as full_name',
                 'u.employee_id',
                 'o.company_name as office',
                 'd.department',
-                DB::raw("? as log_date"),
-                'l.control_number',
-                'l.leave_type',
-                'l.leave_status',
-                DB::raw("NULL as time_in"),
-                DB::raw("NULL as time_out"),
-                'u.supervisor'
+                DB::raw("COALESCE(th.log_date, ?) as log_date"),
+                DB::raw("MAX(l.control_number) AS control_number"),
+                DB::raw("MAX(l.leave_type) AS leave_type"),
+                DB::raw("MAX(l.leave_status) AS leave_status"),
+                'th.time_in',
+                'th.time_out',
+                'u.supervisor',
+                'u.employment_status'
             )
             ->addBinding([$this->fTLdtFrom], 'select')
+            ->leftJoin('time_logs_header as th', function ($join) {
+                $join->on('u.employee_id', '=', 'th.employee_id')
+                    ->where('th.log_date', '=', DB::raw("?"));
+            })
+            ->addBinding($this->fTLdtFrom, 'join')
             ->leftJoin('leaves as l', function ($join) {
                 $join->on('u.employee_id', '=', 'l.employee_id')
-                     ->whereRaw("? BETWEEN l.date_from AND l.date_to", [$this->fTLdtFrom]);
+                    ->whereRaw("? BETWEEN l.date_from AND l.date_to", [$this->fTLdtFrom])
+                    ->whereNotIn('l.leave_status', ['Denied', 'Cancelled']);
             })
             ->leftJoin('departments as d', 'u.department', '=', 'd.department_code')
             ->leftJoin('offices as o', 'u.office', '=', 'o.id')
-            ->whereNotExists(function ($query) {
-                $query->select(DB::raw(1))
-                      ->from('time_logs_header')
-                      ->whereColumn('time_logs_header.employee_id', 'u.employee_id')
-                      ->where('time_logs_header.log_date', '=', DB::raw("?"))
-                      ->addBinding($this->fTLdtFrom);
+            ->when(Auth::user()->id != 1, function ($query) {
+                $query->where(function ($q) {
+                    $q->where('u.employee_id', Auth::user()->employee_id)
+                        ->orWhere('u.supervisor', Auth::user()->employee_id);
+                });
             })
-            ->whereNotIn('l.leave_status', ['Denied', 'Cancelled'])
+
+            ->where('u.employment_status', 'NO LONGER CONNECTED')
             ->where(function ($query) {
-                $query->whereNull('u.is_deleted')
-                      ->orWhere('u.is_deleted', '!=', 1);
+                $query->whereNull('u.is_deleted')->orWhere('u.is_deleted', '!=', 1);
             })
-            ->when(!in_array(Auth::user()->role_type, ['SUPER ADMIN', 'ADMIN']), function ($q) {
-                $q->where(fn($query) => 
-                    $query->where('u.employee_id', Auth::user()->employee_id)
-                          ->orWhere('u.supervisor', Auth::user()->employee_id)
-                );
-            })
-            ->when(!in_array(Auth::user()->id, [1, 2]), fn($q) => $q->where('u.id', '!=', 1))
-            ->when(Auth::user()->is_head == 1, function ($q) {
-                $q->where(fn($query) => 
-                    $query->where('u.employee_id', Auth::user()->employee_id)
-                          ->orWhere('u.supervisor', Auth::user()->employee_id)
-                );
-            }, fn($q) => 
-                $q->where('u.employee_id', Auth::user()->employee_id)
+            ->groupBy(
+                'u.employee_id',
+                'u.name',
+                'o.company_name',
+                'd.department',
+                'th.id',
+                'th.log_date',
+                'th.time_in',
+                'th.time_out',
+                'u.supervisor'
             )
-        )
-        ->orderBy('log_date', 'desc')
-        ->orderBy('full_name', 'asc')
-        ->paginate($this->pageSize);
+            ->orderBy('u.name', 'asc')
+            ->orderBy('log_date', 'desc')
+            ->paginate($this->pageSize);
 
-        return $timeLogs;*/
-
-
-        $timeLogs = DB::table('users as u')
-    ->select(
-        DB::raw("COALESCE(th.id, NULL) as id"),
-        'u.name as full_name',
-        'u.employee_id',
-        'o.company_name as office',
-        'd.department',
-        DB::raw("COALESCE(th.log_date, ?) as log_date"),
-        DB::raw("MAX(l.control_number) AS control_number"),
-        DB::raw("MAX(l.leave_type) AS leave_type"),
-        DB::raw("MAX(l.leave_status) AS leave_status"),
-        'th.time_in',
-        'th.time_out',
-        'u.supervisor'
-    )
-    ->addBinding([$this->fTLdtFrom], 'select')
-    ->leftJoin('time_logs_header as th', function ($join) {
-        $join->on('u.employee_id', '=', 'th.employee_id')
-             ->where('th.log_date', '=', DB::raw("?"));
-    })
-    ->addBinding($this->fTLdtFrom, 'join')
-    ->leftJoin('leaves as l', function ($join) {
-        $join->on('u.employee_id', '=', 'l.employee_id')
-             ->whereRaw("? BETWEEN l.date_from AND l.date_to", [$this->fTLdtFrom])
-             ->whereNotIn('l.leave_status', ['Denied', 'Cancelled']);
-    })
-    ->leftJoin('departments as d', 'u.department', '=', 'd.department_code')
-    ->leftJoin('offices as o', 'u.office', '=', 'o.id')
-    ->where(fn($query) => 
-        $query->where('u.employee_id', Auth::user()->employee_id)
-              ->orWhere('u.supervisor', Auth::user()->employee_id)
-    )
-    ->where(function ($query) {
-        $query->whereNull('u.is_deleted')->orWhere('u.is_deleted', '!=', 1);
-    })
-    ->groupBy(
-        'u.employee_id', 'u.name', 'o.company_name', 'd.department',
-        'th.id', 'th.log_date', 'th.time_in', 'th.time_out', 'u.supervisor'
-    )
-    ->orderBy('u.name', 'asc')
-    ->orderBy('log_date', 'desc')
-    ->paginate($this->pageSize);
-
-return $timeLogs;
-
-
-
-	}
+        return $timeLogs;
+    }
 
 
 
--- resources/views/livewire/records-management/attendance-monitoring.blade.php
+++ resources/views/livewire/records-management/attendance-monitoring.blade.php
@@ -8,20 +8,22 @@
         <!-- FORM start -->
 
         @if (session('status'))
-        <div class="alert alert-success">
-            {{ session('status') }}
-        </div>
+            <div class="alert alert-success">
+                {{ session('status') }}
+            </div>
         @endif
 
-        <div class="px-3 bg-white sm:p-6 shadow {{ isset($actions) ? 'sm:rounded-tl-md sm:rounded-tr-md' : 'sm:rounded-md' }}">
+        <div
+            class="px-3 bg-white sm:p-6 shadow {{ isset($actions) ? 'sm:rounded-tl-md sm:rounded-tr-md' : 'sm:rounded-md' }}">
             <div class="col-span-8 sm:col-span-8 sm:justify-center">
                 <div class="row pb-1 mx-1 inset-shadow">
                     <div class="col-md-2 py-2">
                         <div class="form-floating w-full" id="divfilterEmpOffice">
-                            <select wire:model="fTLOffice" name="fTLOffice" id="fTLOffice" class="form-control border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm mt-1 block w-full">
+                            <select wire:model="fTLOffice" name="fTLOffice" id="fTLOffice"
+                                class="form-control border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm mt-1 block w-full">
                                 <option value="">All Offices</option>
                                 @foreach ($offices as $office)
-                                <option value="{{ $office->id }}">{{ $office->company_name }}</option>
+                                    <option value="{{ $office->id }}">{{ $office->company_name }}</option>
                                 @endforeach
                             </select>
                             <x-jet-label for="fTLOffice" value="{{ __('OFFICE') }}" />
@@ -30,10 +32,12 @@
 
                     <div class="col-md-2 py-2">
                         <div class="form-floating w-full" id="divfTLDept">
-                            <select wire:model="fTLDept" name="fTLDept" id="fTLDept" class="form-control border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm mt-1 block w-full">
+                            <select wire:model="fTLDept" name="fTLDept" id="fTLDept"
+                                class="form-control border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm mt-1 block w-full">
                                 <option value="">All Departments</option>
                                 @foreach ($departments as $department)
-                                <option value="{{ $department->department_code }}">{{ $department->department }}</option>
+                                    <option value="{{ $department->department_code }}">{{ $department->department }}
+                                    </option>
                                 @endforeach
                             </select>
                             <x-jet-label for="fTLDept" value="{{ __('DEPARTMENT') }}" />
@@ -47,7 +51,8 @@
                         </div>
 
                         <div class="flex justify-center items-center">
-                            <x-jet-input wire:model.debounce.500ms="fTLdtFrom" type="date" id="fTLdtFrom" name="fTLdtFrom" placeholder="mm/dd/yyyy" autocomplete="off" class="mx-1 w-full" />
+                            <x-jet-input wire:model.debounce.500ms="fTLdtFrom" type="date" id="fTLdtFrom"
+                                name="fTLdtFrom" placeholder="mm/dd/yyyy" autocomplete="off" class="mx-1 w-full" />
                             {{-- to
 					            <x-jet-input wire:model.debounce.500ms="fTLdtTo" type="date" id="fTLdtTo" name="fTLdtTo" placeholder="mm/dd/yyyy" autocomplete="off" class="mx-1"/> --}}
                         </div>
@@ -56,7 +61,7 @@
                     <div class="col-md-2 py-2"></div>
 
                     <div class="col-md-3 text-center mt-2 py-2">
-                        {{-- @if (Auth::user()->id==1 || Auth::user()->id==8 || Auth::user()->id==58 || Auth::user()->id==287)
+                        {{-- @if (Auth::user()->id == 1 || Auth::user()->id == 8 || Auth::user()->id == 58 || Auth::user()->id == 287)
 				                <div class="form-group btn btn-outline-success d-inline-block shadow-sm p-2 rounded capitalize hover w-75">
 				                    <i class="fas fa-table"></i>
 				                    <span id="exportExcel" class="font-weight-bold">Export to Excel</span>
@@ -72,7 +77,8 @@
                     <div class="col-md-9">
                         <div class="form-inline justify-content-start">
                             <label for="pageSize" class="mr-2">Show:</label>
-                            <select wire:model="pageSize" id="pageSize" class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md">
+                            <select wire:model="pageSize" id="pageSize"
+                                class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md">
                                 <option value="5">5</option>
                                 <option value="10">10</option>
                                 <option value="15">15</option>
@@ -115,74 +121,80 @@
                 </div>
             </div> --}}
 
-            <table id="dataTimeLogs" class="view-detailed-timelogs table table-bordered table-striped sm:justify-center table-hover text-sm">
-                <thead class="thead">
-                    <tr class="dt-head-center">
-                        <th class="py-1">Name</th>
-                        <th class="py-1" style="width: 8%">Emp. ID</th>
-                        {{-- <th class="py-1">Office</th> --}}
-                        {{-- <th class="py-1" style="width: 20%">Department</th> --}}
-                        <th class="py-1" style="width: 8%">Date</th>
-                        <th class="py-1" style="width: 8%">Day</th>
-                        <th class="py-1" style="width: 8%">Time In</th>
-                        <th class="py-1" style="width: 8%">Time Out</th>
-                        {{-- <th class="py-1" style="width: 12%">Leave</th> --}}
-                        <th class="py-1" style="width: 12%">Leave Control #</th>
-                        <th class="py-1">Supervisor</th>
-                        <th class="py-1" style="width: 12%">Status</th>
-                    </tr>
-                </thead>
-                <tbody class="data custom-text-xs" id="viewEmployee">
-                    @if ($timeLogs->isNotEmpty())
-                    @foreach ($timeLogs as $record)
-                    <tr id="{{ $record->id }}">
-                        @if (url('/')=='http://localhost')
-                        <td>xxx, xxx x.</td>
-                        @else
-                        <td>{{ $record->full_name }}</td>
-                        @endif
-                        {{-- <td>{{ $record->id }}</td> --}}
-                        <td>{{ $record->employee_id }}</td>
-                        {{-- <td>{{ $record->office }}</td> --}}
-                        {{-- <td>{{ $record->department }}</td> --}}
-                        <td>{{ date('m/d/Y', strtotime($record->log_date)) }}</td>
-                        <td>{{ date('D', strtotime($record->log_date)) }}</td>
-                        <td>{{ $record->time_in ? date('g:i A', strtotime($record->time_in)) : '' }}</td>
-                        <td>{{ $record->time_out ? date('g:i A', strtotime($record->time_out)) : '' }}</td>
-                        <td>{{ $record->control_number ?? '' }}</td>
-                        @if (url('/')=='http://localhost')
-                        <td>xxx, xxx x.</td>
-                        @else
-                        <td>{{ $record->supervisor }}</td>
-                        @endif
-                        <td>{{ $record->leave_type ?? ($record->time_in === null ? '-' : '') }}</td>
-                    </tr>
-                    @endforeach
-                    @else
-                    <td colspan="9">No Matching Records Found</td>
-                    @endif
-                </tbody>
-            </table>
-            <div class="d-flex justify-content-end">
-                <div class="form-inline">
-                    <label for="pageSize" class="mr-2">Show:</label>
-                    <select wire:model="pageSize" id="pageSize" class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md">
-                        <option value="5">5</option>
-                        <option value="10">10</option>
-                        <option value="15">15</option>
-                        <option value="25">25</option>
-                        <option value="50">50</option>
-                    </select>
-                    <span class="mx-2">entries</span>
-                    {{-- </div>
+                    <table id="dataTimeLogs"
+                        class="view-detailed-timelogs table table-bordered table-striped sm:justify-center table-hover text-sm">
+                        <thead class="thead">
+                            <tr class="dt-head-center">
+                                <th class="py-1">Name</th>
+                                <th class="py-1" style="width: 8%">Emp. ID</th>
+                                {{-- <th class="py-1">Office</th> --}}
+                                {{-- <th class="py-1" style="width: 20%">Department</th> --}}
+                                <th class="py-1" style="width: 8%">Date</th>
+                                <th class="py-1" style="width: 8%">Day</th>
+                                <th class="py-1" style="width: 8%">Time In</th>
+                                <th class="py-1" style="width: 8%">Time Out</th>
+                                {{-- <th class="py-1" style="width: 12%">Leave</th> --}}
+                                <th class="py-1" style="width: 12%">Leave Control #</th>
+                                <th class="py-1">Supervisor</th>
+                                <th class="py-1" style="width: 12%">Status</th>
+                                <th class="py-1" style="width: 12%">Employment Status</th>
+                            </tr>
+                        </thead>
+                        <tbody class="data custom-text-xs" id="viewEmployee">
+                            @if ($timeLogs->isNotEmpty())
+                                @foreach ($timeLogs as $record)
+                                    <tr id="{{ $record->id }}">
+                                        @if (url('/') == 'http://localhost')
+                                            <td>xxx, xxx x.</td>
+                                        @else
+                                            <td>{{ $record->full_name }}</td>
+                                        @endif
+                                        {{-- <td>{{ $record->id }}</td> --}}
+                                        <td>{{ $record->employee_id }}</td>
+                                        {{-- <td>{{ $record->office }}</td> --}}
+                                        {{-- <td>{{ $record->department }}</td> --}}
+                                        <td>{{ date('m/d/Y', strtotime($record->log_date)) }}</td>
+                                        <td>{{ date('D', strtotime($record->log_date)) }}</td>
+                                        <td>{{ $record->time_in ? date('g:i A', strtotime($record->time_in)) : '' }}
+                                        </td>
+                                        <td>{{ $record->time_out ? date('g:i A', strtotime($record->time_out)) : '' }}
+                                        </td>
+                                        <td>{{ $record->control_number ?? '' }}</td>
+                                        @if (url('/') == 'http://localhost')
+                                            <td>xxx, xxx x.</td>
+                                        @else
+                                            <td>{{ $record->supervisor }}</td>
+                                        @endif
+                                        <td>{{ $record->leave_type ?? ($record->time_in === null ? '-' : '') }}</td>
+                                        <td>{{ $record->employment_status }}</td>
+                                    </tr>
+                                @endforeach
+                            @else
+                                <td colspan="9">No Matching Records Found</td>
+                            @endif
+                        </tbody>
+                    </table>
+                    <div class="d-flex justify-content-end">
+                        <div class="form-inline">
+                            <label for="pageSize" class="mr-2">Show:</label>
+                            <select wire:model="pageSize" id="pageSize"
+                                class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md">
+                                <option value="5">5</option>
+                                <option value="10">10</option>
+                                <option value="15">15</option>
+                                <option value="25">25</option>
+                                <option value="50">50</option>
+                            </select>
+                            <span class="mx-2">entries</span>
+                            {{-- </div>
 						    <div class="d-flex align-items-center"> --}}
-                    {{ $timeLogs->links('pagination.custom') }}
+                            {{ $timeLogs->links('pagination.custom') }}
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-</div>
 </div>
 
 <!-- =========================================== -->
